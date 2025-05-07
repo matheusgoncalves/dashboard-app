349515252f74cfa6e0efdd3a66719dd640b7089e
Matheus <msgoncalves@inf.ufpel.edu.br>
2025-05-07 17:05:36 -0300

    configura a página de clientes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
+import Table from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <h1>Customers Page</h1>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <Suspense key={query} fallback={<div>Loading...</div>}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
